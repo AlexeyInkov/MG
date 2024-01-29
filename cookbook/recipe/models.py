b8e046bb6184ce098d20193817747aed8ba25a72
Alexey Inkov <alex0982@mail.ru>
2024-01-29 20:39:51 +0300

    create models

--- cookbook/recipe/models.py
+++ cookbook/recipe/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    count_recipe = models.IntegerField(default=0)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class RecipeItem(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_weight = models.IntegerField(default=0)
